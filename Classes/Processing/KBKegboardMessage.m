55155c1111e27d5d7158bd771f26d479d14b5fb3
John Boiles <johnaboiles@gmail.com>
2013-06-14 23:38:49 +0700

    Added KBKegboardCommand for sending messages to the Kegboard. Cleaned up KBKegboardMessage. Wrote some tests.

--- Classes/Processing/KBKegboardMessage.m
+++ Classes/Processing/KBKegboardMessage.m
@@ -5,60 +5,82 @@
 // Created by John Boiles on 9/27/10.
 // Copyright 2010 Yelp. All rights reserved.
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
 // Made ARC compliant *crosses fingers* for use with KegCop on 28MAY13
 //
-// TODO - capin
-// - clearly define what class does
 
 #import "KBKegboardMessage.h"
+#import "KBKegboardUtils.h"
+#import "KBKegboardCommand.h"
 
 char *const KBSP_PREFIX = kKBSP_PREFIX;
 char *const KBSP_TRAILER = kKBSP_TRAILER;
 
-@interface KBKegboardMessage ()
-@property (assign, nonatomic) NSTimeInterval timeStamp;
-- (void)parsePayload:(char *)payload length:(NSUInteger)length;
-- (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length;
-@end
 
 @implementation KBKegboardMessage
+@synthesize timeStamp=_timeStamp;
 
-@synthesize messageId=_messageId, timeStamp=_timeStamp;
++ (NSUInteger)messageId {
+    NSAssert(NO, @"Subclasses must implement messageId");
+    return 0;
+}
+
+- (NSUInteger)messageId {
+    return [[self class] messageId];
+}
 
 + (id)messageWithId:(NSUInteger)messageId payload:(char *)payload length:(NSUInteger)length timeStamp:(NSTimeInterval)timeStamp {
     KBKegboardMessage *message = nil;
+    // Any time you add a new KBKegboardMessage subclass, you should add it here
     switch (messageId) {
         case KB_MESSAGE_ID_HELLO:
-            
-            // capin - removed "autorelease" from end of "message = ..." to make statement work with ARC
-            
-            message = [[KBKegboardMessageHello alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageHello alloc] initWithPayload:payload length:length];
             break;
         case KB_MESSAGE_ID_BOARD_CONFIGURATION:
-            message = [[KBKegboardMessageBoardConfiguration alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageBoardConfiguration alloc] initWithPayload:payload length:length];
             break;
         case KB_MESSAGE_ID_METER_STATUS:
-            message = [[KBKegboardMessageMeterStatus alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageMeterStatus alloc] initWithPayload:payload length:length];
             break;
         case KB_MESSAGE_ID_TEMPERATURE_READING:
-            message = [[KBKegboardMessageTemperatureReading alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageTemperatureReading alloc] initWithPayload:payload length:length];
             break;
         case KB_MESSAGE_ID_OUTPUT_STATUS:
-            message = [[KBKegboardMessageOutputStatus alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageOutputStatus alloc] initWithPayload:payload length:length];
             break;
         case KB_MESSAGE_ID_AUTH_TOKEN:
-            message = [[KBKegboardMessageAuthToken alloc] initWithMessageId:messageId payload:payload length:length];
+            message = [[KBKegboardMessageAuthToken alloc] initWithPayload:payload length:length];
+            break;
+        case KB_MESSAGE_ID_PING:
+            message = [[KBKegboardCommandPing alloc] initWithPayload:payload length:length];
+            break;
+        case KB_MESSAGE_ID_SET_OUTPUT:
+            message = [[KBKegboardCommandSetOutput alloc] initWithPayload:payload length:length];
             break;
     }
-    message.timeStamp = timeStamp;
+    message->_timeStamp = timeStamp;
     if (!message) NSLog(@"Got unknown message with ID %x", messageId);
     return message;
 }
 
-- (id)initWithMessageId:(NSUInteger)messageId payload:(char *)payload length:(NSUInteger)length {
+- (id)initWithPayload:(char *)payload length:(NSUInteger)length {
     if ((self = [super init])) {
-        _messageId = messageId;
-        [self parsePayload:payload length:length];
+        // Some messages may have no payload
+        if (length > 0) {
+            [self parsePayload:payload length:length];
+        }
     }
     return self;
 }
@@ -66,9 +88,9 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 - (void)parsePayload:(char *)payload length:(NSUInteger)length {
     NSUInteger index = 0;
     while (index < length) {
-        NSUInteger tag = [KBKegboardMessage parseUInt8:&payload[index]];
+        NSUInteger tag = [KBKegboardUtils parseUInt8:&payload[index]];
         index++;
-        NSUInteger length = [KBKegboardMessage parseUInt8:&payload[index]];
+        NSUInteger length = [KBKegboardUtils parseUInt8:&payload[index]];
         index++;
         if (![self parsePayload:&payload[index] forTag:tag length:length]) NSLog(@"Failed to parse tag: %d", tag);
         index += length;
@@ -80,65 +102,20 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
     return NO;
 }
 
-- (NSString *)description {
-    return [NSString stringWithFormat:@"KBKegboardMessage with messageId %u", _messageId];
-}
-
-+ (NSString *)parseString:(char *)stringData length:(NSUInteger)length {
-    // NOTE: Kegboard docs say strings are null terminated, but in the latest version (as of 7/29/2010), they are not.
-    // To be safe, check for null termination
-    if (!stringData[length - 1]) length -= 1;
-    
-    //
-    // capin - removed "autorelease" from end of "message = ..." to make statement work with ARC
-    //
-    
-    return [[NSString alloc] initWithBytes:stringData length:length encoding:NSASCIIStringEncoding];
-}
-
-+ (NSUInteger)parseUInt32:(char *)data length:(NSUInteger)length {
-    NSUInteger output = 0;
-    for (NSInteger i = 0; i < length; i++) {
-        output += (unsigned char)data[i] << (i * 8);
-    }
-    return output;
-}
-
-+ (NSUInteger)parseUInt8:(char *)data {
-    return [KBKegboardMessage parseUInt32:data length:1];
-}
-
-+ (NSUInteger)parseUInt16:(char *)data {
-    return [KBKegboardMessage parseUInt32:data length:2];
-}
-
-+ (NSUInteger)parseUInt32:(char *)data {
-    return [KBKegboardMessage parseUInt32:data length:4];
-}
-
-+ (NSInteger)parseInt32:(char *)data {
-    return (NSInteger)[KBKegboardMessage parseUInt32:data];
-}
-
-+ (double)parseTemp:(char *)data {
-    NSInteger tempInteger = [KBKegboardMessage parseInt32:data];
-    return ((double)tempInteger / 1000000);
-}
-
-+ (BOOL)parseBool:(char *)data {
-    if (data[0] & 0xFF) return YES;
-    else return NO;
-}
-
 @end
+
 
 @implementation KBKegboardMessageHello
 @synthesize protocolVersion=_protocolVersion;
 
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_HELLO;
+}
+
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            _protocolVersion = [KBKegboardMessage parseUInt16:data];
+            _protocolVersion = [KBKegboardUtils parseUInt16:data];
             break;
         default:
             return NO;
@@ -156,35 +133,29 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 @implementation KBKegboardMessageBoardConfiguration
 @synthesize boardName=_boardName, baudRate=_baudRate, updateInterval=_updateInterval, watchdogTimeout=_watchdogTimeout;
 
-- (void)dealloc {
-    //[_boardName dealloc];
-   // [super dealloc];
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_BOARD_CONFIGURATION;
 }
 
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            
-            // capin - removed "retain" from end of "_boardName = ..." to make statement work with ARC
-
-            
-            _boardName = [KBKegboardMessage parseString:data length:length];
+            _boardName = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x02:
-            _baudRate = [KBKegboardMessage parseUInt16:data];
+            _baudRate = [KBKegboardUtils parseUInt16:data];
             break;
         case 0x03:
-            _updateInterval = [KBKegboardMessage parseUInt16:data];
+            _updateInterval = [KBKegboardUtils parseUInt16:data];
             break;
         case 0x04:
-            _watchdogTimeout= [KBKegboardMessage parseUInt16:data];
+            _watchdogTimeout= [KBKegboardUtils parseUInt16:data];
             break;
         default:
             return NO;
     }
     return YES;
 }
-
 
 - (NSString *)description {
     return [NSString stringWithFormat:@"KBKegboardMessageBoardConfiguration with boardName %@ baudRate %u updateInterval %u watchdogTimeout %u", _boardName, _baudRate, _updateInterval, _watchdogTimeout];
@@ -196,24 +167,17 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 @implementation KBKegboardMessageMeterStatus
 @synthesize meterName=_meterName, meterReading=_meterReading;
 
-- (void)dealloc {
-    
-    // capin - commented out the below lines of code to make class work with ARC
-    
-   // [_meterName dealloc];
-   // [super dealloc];
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_METER_STATUS;
 }
 
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            
-            // capin - removed "retain" from "_meterName = ..." to make ARC compliant
-            
-            _meterName = [KBKegboardMessage parseString:data length:length];
+            _meterName = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x02:
-            _meterReading = [KBKegboardMessage parseUInt32:data];
+            _meterReading = [KBKegboardUtils parseUInt32:data];
             break;
         default:
             return NO;
@@ -231,22 +195,17 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 @implementation KBKegboardMessageTemperatureReading
 @synthesize sensorName=_sensorName, sensorReading=_sensorReading;
 
-- (void)dealloc {
-    
-    // capin - commented out below lines of code to make ARC compliant
-    
-    // [_sensorName dealloc];
-    // [super dealloc];
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_TEMPERATURE_READING;
 }
 
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            // capin - removed "retain" from "_sensorName = ..." statement for ARC compliance
-            _sensorName = [KBKegboardMessage parseString:data length:length];
+            _sensorName = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x02:
-            _sensorReading = [KBKegboardMessage parseTemp:data];
+            _sensorReading = [KBKegboardUtils parseTemp:data];
             break;
         default:
             return NO;
@@ -264,24 +223,17 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 @implementation KBKegboardMessageOutputStatus
 @synthesize outputName=_outputName, outputReading=_outputReading;
 
-- (void)dealloc {
-    
-    // capin - commented out below lines of code to make ARC compliant
-    
-    // [_outputName dealloc];
-    // [super dealloc];
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_OUTPUT_STATUS;
 }
 
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            
-            // capin - removed "retain" from "_outputName = ..." statement for ARC compliance
-            
-            _outputName = [KBKegboardMessage parseString:data length:length];
+            _outputName = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x02:
-            _outputReading = [KBKegboardMessage parseBool:data];
+            _outputReading = [KBKegboardUtils parseBool:data];
             break;
         default:
             return NO;
@@ -298,33 +250,21 @@ char *const KBSP_TRAILER = kKBSP_TRAILER;
 
 @implementation KBKegboardMessageAuthToken
 @synthesize deviceName=_deviceName, token=_token, status=_status;
-- (void)dealloc {
-    
-    // capin - commented out below lines of code to make ARC compliant
-    
-    // [_deviceName dealloc];
-    // [_token dealloc];
-    // [super dealloc];
+
++ (NSUInteger)messageId {
+    return KB_MESSAGE_ID_AUTH_TOKEN;
 }
 
 - (BOOL)parsePayload:(char *)data forTag:(NSUInteger)tag length:(NSUInteger)length {
     switch (tag) {
         case 0x01:
-            
-            // capin - removed "retain" from "_outputName = ..." statement for ARC compliance
-
-            
-            _deviceName = [KBKegboardMessage parseString:data length:length];
+            _deviceName = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x02:
-            
-            // capin - removed "retain" from "_outputName = ..." statement for ARC compliance
-
-            
-            _token = [KBKegboardMessage parseString:data length:length];
+            _token = [KBKegboardUtils parseString:data length:length];
             break;
         case 0x03:
-            _status = [KBKegboardMessage parseBool:data];
+            _status = [KBKegboardUtils parseBool:data];
             break;
         default:
             return NO;

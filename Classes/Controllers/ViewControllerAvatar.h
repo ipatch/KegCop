f08edf83df883fbedfb20a20ce3d767b2dc5148c
capin <capin@OSX-VM.local>
2014-10-28 16:37:52 -0500

    add .gitignore to project

---
+++ Classes/Controllers/ViewControllerAvatar.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerAvatar.h
+//  KegCop
+//
+//  Created by capin on 10/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewControllerAvatar : NSObject
+
+@end
---
+++ Classes/Controllers/ViewControllerAvatar.m
@@ -0,0 +1,13 @@
+//
+//  ViewControllerAvatar.m
+//  KegCop
+//
+//  Created by capin on 10/28/14.
+//
+//
+
+#import "ViewControllerAvatar.h"
+
+@implementation ViewControllerAvatar
+
+@end

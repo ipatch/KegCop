9a610bd62c8ad97b496d2ef3ae696021188dab50
capin <capin@OSX-VM.local>
2014-11-05 22:59:28 -0600

    add ViewControllerAvatar.swift

---
+++ Classes/Controllers/HUMConfirmationViewController.h
@@ -0,0 +1,13 @@
+//
+//  HUMConfirmationViewController.h
+//  KegCop
+//
+//  Created by capin on 11/5/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUMConfirmationViewController : UIViewController
+
+@end

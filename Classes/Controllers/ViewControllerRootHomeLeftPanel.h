44513e6fd2f6079d8e27b83214f69a9b883111a4
capin <capin@yosemite-VM.local>
2015-03-08 16:44:12 -0700

    trying to get slide out menu working

--- Classes/Controllers/ViewControllerRootHomeLeftPanel.h
+++ Classes/Controllers/ViewControllerRootHomeLeftPanel.h
@@ -7,14 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ViewControllerRootHome.h"
 
+@class ViewControllerRootHome;
 @protocol ViewControllerRootHomeLeftPanelDelegate <NSObject>
+- (void) movePanelToOriginalPosition: (ViewControllerRootHome *) sender;
 
 @end
 
-@interface ViewControllerRootHomeLeftPanel : UIViewController <UITableViewDataSource, UITableViewDelegate>
-
-@property (nonatomic, assign) id<ViewControllerRootHomeLeftPanelDelegate> delegate;
+@interface ViewControllerRootHomeLeftPanel : UIViewController                     <MovePanelToOriginalPositionDelegate, UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, weak) id<ViewControllerRootHomeLeftPanelDelegate> delegate;
+//@property (nonatomic, assign) id<ViewControllerRootHomeLeftPanelDelegate> delegate;
 
 // need to find a UI element that lists elements in a column.
 @property (strong, nonatomic) UITableView *tableView;
--- Classes/Controllers/ViewControllerRootHomeLeftPanel.m
+++ Classes/Controllers/ViewControllerRootHomeLeftPanel.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewControllerRootHomeLeftPanel.h"
+#import "ViewControllerRootHome.h"
+#import "ViewControllerRootHomeCenter.h"
+
+#define SLIDE_TIMING .25
 
 @interface ViewControllerRootHomeLeftPanel ()
+
+@property (nonatomic, weak) IBOutlet UITableViewCell *cellMain;
+
+@property (nonatomic, strong) ViewControllerRootHome *vcrh;
+
+//@property (nonatomic, weak) IBOutlet UITableView *tableView;
 
 @end
 
@@ -45,12 +55,16 @@
     // Do any additional setup after loading the view.
     
     
+    // tableView cell options
     _options = [[NSMutableArray alloc] initWithObjects:@"test", @"Manage Accounts", @"Logoff", nil];
     
     
     self.tableView = [self makeTableView];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Options"];
     [self.view addSubview:self.tableView];
+    
+    
+    
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -79,6 +93,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - table view delegate
+
+- (void)showViewControllerRootHomeCenter {
+    
+    
+    
+//    [self.tableView registerNib:[UINib nibWithNibName:@"LeftPanelViewController" bundle:nil]  forCellReuseIdentifier:@"Manage Accounts"];
+    
+//    [self setContentViewController:vcrhc animated:YES completion:nil];
+//    [self.ViewControllerRootHomeLeftPanel.view removeFromSuperView];
+//    self.ViewControllerRootHomeLeftPanel = nil;
+
+    //    static NSString *cellMainNibID = @"LeftPanelViewController";
+    
+//    _cellMain = [_tableView dequeueReusableCellWithIdentifier:cellMainNibID];
+   }
+
+//- (void) movePanelToOriginalPosition: (ViewControllerRootHome *) sender {
+//    NSLog(@"Delegates are great!");
+//    [UIView animateWithDuration:SLIDE_TIMING delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+//        _viewControllerRootHomeCenter.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+//    }
+//                     completion:^(BOOL finished) {
+//                         if (finished) {
+//                             [self resetMainView];
+//                         }
+//                     }];
+//}
+
+
+//-(void)resetMainView {
+//    // remove left and right views, and reset variables, if needed
+//    if (_viewControllerRootHomeLeftPanel != nil) {
+//        [self.viewControllerRootHomeLeftPanel.view removeFromSuperview];
+//        self.viewControllerRootHomeLeftPanel = nil;
+//        _viewControllerRootHomeCenter.leftButton.tag = 1;
+//        _viewControllerRootHomeCenter.hamMenu.tag = 1;
+//        self.showingLeftPanel = NO;
+//    }
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath: (NSIndexPath *)indexPath {
     
     NSString *currentString = [tableView cellForRowAtIndexPath:indexPath].textLabel.text;
@@ -88,6 +142,48 @@
         [self dismissViewControllerAnimated:YES completion:nil];
     }
     
+    if ([currentString isEqualToString:@"Manage Accounts"]) {
+        NSLog(@"Manage Accounts button pressed");
+        
+        // do any additional checks / loads for managing accounts.
+        
+        
+//        [[self presentingViewController] dismissViewControllerAnimated:NO completion:nil];
+        
+        
+        
+        
+        
+//        [tableView dequeueReusableCellWithIdentifier:@"Manage Accounts"];
+        
+        
+        
+        
+        
+        
+        
+        //        static NSString *CellIdentifier = @"Cell";
+        
+        
+        
+        
+//        [_delegate movePanelToOriginalPosition];
+        
+//        static NSString *centerNib = @"CenterViewController";
+        
+//        _parentViewController = [tableView dequeueReusableCellWithIdentifier:centerNib];
+//        if (_cellMain == nil) {
+//            [[NSBundle mainBundle] loadNibNamed:@"CenterViewController" owner:self options:nil];
+
+        
+        // the below code was presenting a new view controller not hiding the left pane :(
+//        UIViewController *vcrhc = [[UIViewController alloc] initWithNibName:@"CenterViewController" bundle:nil];
+//        
+//        [self presentViewController:vcrhc animated:YES completion:nil];
+        
+    }
+//    return cell;
+
 }
 
 /*

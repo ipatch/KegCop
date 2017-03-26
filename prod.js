207555e0ed6dad0eaeb0c4e99dec07590fd7aceb
ipatch <chris.r.jones.1983@gmail.com>
2017-03-25 20:24:34 -0500

    tshooting

--- prod.js
+++ prod.js
@@ -1,27 +1,30 @@
-module.exports = function (env) {
-  return {
-    output: {
-        path: path.join(__dirname, '/../docs/assets'),
-        filename: '[name].bundle.js',
-        publicPath: publicPath,
-        sourceMapFilename: '[name].map'
-    },
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            beautify: false,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                screw_ie8: true
-            },
-            comments: false
-        })
-    ]
-  }
+const webpackMerge = require('webpack-merge');
+
+const commonConfig = require('./base.js');
+
+module.exports = function(env) {
+    return webpackMerge(commonConfig(), {
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            }),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                beautify: false,
+                mangle: {
+                    screw_ie8: true,
+                    keep_fnames: true
+                },
+                compress: {
+                    screw_ie8: true
+                },
+                comments: false
+            })
+        ]
+    })
 }

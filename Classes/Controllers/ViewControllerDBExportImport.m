0c7643970c3a10494646fae80bf5939001a04c56
ipatch <chris.r.jones.1983@gmail.com>
2018-02-18 15:15:08 -0600

    "Your butt is mine." -- Michael Jackson, Bad

--- Classes/Controllers/ViewControllerDBExportImport.m
+++ Classes/Controllers/ViewControllerDBExportImport.m
@@ -212,10 +212,7 @@
     // the below string will include the entire path, not just the file name
     NSString *filename = [documents stringByAppendingPathComponent:[NSString stringWithFormat:@"KegCop-users-%@.csv",idfv]];
     
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    
-    // afnetworking class
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    NSData *data = [NSData dataWithContentsOfFile:filename];
     
     NSURL *url;
 #ifdef DEBUG
@@ -225,21 +222,64 @@
     // want to use this variable on RELEASE build
     url = [NSURL URLWithString:@"http://kegcop.chrisrjones.com/api/csv_files/"];
 #endif
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
-    NSURL *filePath = [NSURL fileURLWithPath:filename];
+    NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:[NSString stringWithString:filename] parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+            [formData appendPartWithFileData:data name:[NSString stringWithFormat:@"csv_file"] fileName:[NSString stringWithFormat:@"KegCop-users-%@.csv",idfv] mimeType:@"application/octet-stream"];
+        } error:nil];
     
-    NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithRequest:request fromFile:filePath progress:nil completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
-        if (error) {
-            NSLog(@"Error: %@", error);
-        } else {
-            NSLog(@"Success: %@ %@", response, responseObject);
-        }
-    }];
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    NSURLSessionUploadTask *uploadTask;
+    uploadTask = [manager uploadTaskWithStreamedRequest:request progress:nil
+                                      completionHandler:^(NSURLResponse * _Nonnull response, id _Nullable responseObject, NSError * _Nullable error) {
+                                          if (error) {
+                                              NSLog(@"Error: %@", [response description]);
+                                          } else {
+                                              NSLog(@"%@ %@", response, responseObject);
+                                          }
+                                      }];
     [uploadTask resume];
-    
-    
-    
+}
+
+////NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:@"http://example.com/upload" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+////    [formData appendPartWithFileURL:[NSURL fileURLWithPath:@"file://path/to/image.jpg"] name:@"file" fileName:@"filename.jpg" mimeType:@"image/jpeg" error:nil];
+////} error:nil];
+//
+//
+//
+//NSURLSessionUploadTask *uploadTask;
+//uploadTask = [manager
+//              uploadTaskWithStreamedRequest:request
+//              progress:^(NSProgress * _Nonnull uploadProgress) {
+//                  // This is not called back on the main queue.
+//                  // You are responsible for dispatching to the main queue for UI updates
+//                  dispatch_async(dispatch_get_main_queue(), ^{
+//                      //Update the progress view
+//                      [progressView setProgress:uploadProgress.fractionCompleted];
+//                  });
+//              }
+//              completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+//                  if (error) {
+//                      NSLog(@"Error: %@", error);
+//                  } else {
+//                      NSLog(@"%@ %@", response, responseObject);
+//                  }
+//              }];
+//
+//[uploadTask resume];
+
+
+
+
+
+
+
+
+
+
+
+
 // LEGACY
     
 //    NSData *data = [NSData dataWithContentsOfFile:filename];
@@ -260,7 +300,6 @@
 //    [httpClient enqueueHTTPRequestOperation:operation];
 //
 // END - LEGACY
-}
 
 #pragma mark - delegate methods for CHCSVParser
 -(void) parserDidBeginDocument:(CHCSVParser *)parser {

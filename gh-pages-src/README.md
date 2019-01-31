170a2455a2c34e63cbc9e1b898d153d912e5cbf3
Chris Jones <chris.r.jones.1983@gmail.com>
2019-01-30 19:53:28 -0600

    update project settings for github-pages-src app

---
+++ gh-pages-src/README.md
@@ -0,0 +1,15 @@
+## GitHub Pages source directory
+
+The contents of this directory
+
+```conf
+./KegCop/gh-pages-src
+```
+
+contain the source the below website.
+
+```conf
+https://ipatch.github.io/KegCop
+```
+
+The site is rather primitive right now, but uses webpack to generate a bundle and inject the bundle into a div contained in the **index.html**.
---
+++ gh-pages-src/config/webpack.dev.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  mode: "development",
+  devtool: 'source-map',
+  output: {
+    filename: "app.bundle.js",
+    // `resolve` will construct an absolute path
+    path: path.resolve(__dirname, "../dist"),
+  },
+  devServer: {
+    overlay: true, // print errors in browser
+    host: '0.0.0.0',
+    useLocalIp: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          ExtractCssChunks.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.(jpe?g|gif|png|bmp)$/,
+        use: [
+          {
+            // NOTE file-loader can also be used to work with the below font types as well
+            // woff,woff2,eot,ttf,otf
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractCssChunks({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      hot: true,
+      orderWarning: true,
+      reloadAll: true,
+      cssModules: true
+    })
+  ]
+}

8a27fb39a3ca83f5b8faf9fd61f070c878a0c472
Chris Jones <chris.r.jones.1983@gmail.com>
2019-03-19 11:15:55 -0500

    relearning how to add images with webpack *shrugs*

--- gh-pages-src/src/index.js
+++ gh-pages-src/src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 // import KegCopLogoSrc from './images/kegcop-logo-no-gradient.svg';
-import KegCopLogoSrc from './images/kegcop-logo-neo-neo-03.svg';
+// import KegCopLogoSrc from './images/kegcop-logo-neo-neo-03.svg';
 
 function addLogo() {
   let kegCopBackgroundLogoDiv = document.createElement('div');
@@ -13,33 +13,52 @@ function addLogo() {
   return kegCopLogo
 }
 
+// document.body.appendChild(addLogo());
 
-// TODO: exp with adding a <svg> tag to the DOM to placed within an HTML document.
-
-// 1. create an svg element
-// 2. assign an ID to the newly created svg element
-// 3. see if a imported SVG can be appended to existing svg element
-// 4. objective: trying to avoid using the <img> tag
-
-// EXP
-// alert('wtf');
-//
-// 100% working with webpack
-let blahblahDiv = document.createElement('div');
-blahblahDiv.id = 'blablah-text';
-let textNode = document.createTextNode("some fancy 42 text.");
-blahblahDiv.appendChild(textNode);
-document.body.appendChild(blahblahDiv);
-
-// 100% working with webpack
+// 100% working with webpack | add div to DOM and add text to div
 let newdiv = document.createElement('div');
 newdiv.appendChild(document.createTextNode('text added from index.js using JavaScript'));
 newdiv.id = 'mr-fancy-new-div';
 document.body.appendChild(newdiv);
 
+// EXP > sanity check
+// alert('wtf');
+//
+
+// 1. create an svg element
+// 2. assign an ID to the newly created svg element
+// 3. see if a imported SVG can be appended to existing svg element
+// 4. objective: avoid using the <img> tag
+
+// setup namespace for SVG
+var svgns = 'http://www.w3.org/2000/svg';
+var svg = document.createElementNS(svgns, 'svg');
+document.body.appendChild(svg);
+svg.setAttribute('version', '1.1');
+svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+// baseProfile
+// width
+// height
+
+// bring in external svg
+var use1 = document.createElementNS(svgns, "use");
+use1.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', './images/purple-circle-plain.svg#circle-purple'); 
+
+svg.appendChild(use1);
+
+// let svgElement = document.createElement('svg');
+// document.body.appendChild(svgElement);
+// var svg = document.getElementsByTagName('svg')[0];
+// var newElement1 = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+// var useElement = document.createElement('use');
+// useElement.href='./images/purple-circle-plain.svg#circle-purple';
+// newElement1.append(useElement);
+// svg.appendChild(newElement1);
+
+
 // 1. An svg tag must be added to the DOM first!
-let svgElement = document.createElement('svg');
-document.body.appendChild(svgElement);
+// let svgElement = document.createElement('svg');
+// document.body.appendChild(svgElement);
 // var svg = document.getElementsByTagName('svg')[0]; //Get svg element
 // var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
 // newElement.setAttribute("d","M 0 0 L 10 10"); //Set path's data
@@ -57,16 +76,16 @@ document.body.appendChild(svgElement);
 // newElement1.append(useElement);
 // svg1.appendChild(newElement1);
 
-var useElement = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+// var useElement = document.createElementNS('http://www.w3.org/2000/svg', 'use');
 
 // NOTE: only seems to reference via URL ...wtf?
-useElement.setAttributeNS(
-	'http://www.w3.org/1999/xlink', // xline NS URI
-	'href',
-	// './images/kegcop-logo-neo-neo-03.svg#kegcop-logo-root');
-	'./images/purple-circle.svg#svg8')
+// useElement.setAttributeNS(
+// 	'http://www.w3.org/1999/xlink', // xline NS URI
+// 	'href',
+// 	// './images/kegcop-logo-neo-neo-03.svg#kegcop-logo-root');
+// 	'./images/purple-circle.svg#svg8')
 
-document.querySelector('svg').appendChild(useElement);
+// document.querySelector('svg').appendChild(useElement);
 
 
 
@@ -74,7 +93,7 @@ document.querySelector('svg').appendChild(useElement);
 // document.svg.appendChild(KegCopLogoSrc);
 
 // var svg = document.getElementsByTagName('svg')[0]; //Get svg element
-var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'g'); //Create a path in SVG's namespace
+// var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'g'); //Create a path in SVG's namespace
 // newElement.setAttribute("d","M 0 0 L 10 10"); //Set path's data
 // newElement.style.stroke = "#000"; //Set stroke colour
 // newElement.style.strokeWidth = "5px"; //Set stroke width
@@ -110,4 +129,4 @@ var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'g'); //
 // svgElKegcopLogo.appendChild(svgKegCopLogoSVG);
 // document.body.appendChild(svgElKegcopLogo);
 
-// document.body.appendChild(addLogo());
+

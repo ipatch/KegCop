fd285430bec05a21a3873172ba2dd87ff3bd551c
Chris Jones <chris.r.jones.1983@gmail.com>
2019-09-20 14:52:28 -0500

    update conditional logic for running GA based domain in URL bar of browser DOM

--- src/utils/google-analytics.js
+++ src/utils/google-analytics.js
@@ -10,18 +10,6 @@ function loadGoogleAnalytics() {
   s.parentNode.insertBefore(ga, s);
 }
 
-// NOTE: only load GA on production domain
-// if (document.location.hostname.search('https://ipatch.github.io/KegCop') !== -1) {
-
-//   loadGoogleAnalytics();
-
-//   window.dataLayer = window.dataLayer || [];
-
-//   function gtag(){dataLayer.push(arguments);}
-
-//   gtag('js', new Date());
-
-//   gtag('config', 'UA-43223650-1');
 
 // } else {
 //   (document.location.hostname.search('http:/locallhost') !== -1)
@@ -30,8 +18,30 @@ function loadGoogleAnalytics() {
 
 if (document.location.hostname.search('https://ipatch.github.io/KegCop') === -1) {
   console.log("app is running from https://ipatch.github.io/KegCop domain");
+
+  loadGoogleAnalytics();
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+  gtag('config', 'UA-43223650-1');
+
 } else if (document.location.hostname.search('http://localhost') === -1) {
   console.log('hotdogs for all');
+
+  // loadGoogleAnalytics();
+
+  // window.dataLayer = window.dataLayer || [];
+
+  // function gtag(){dataLayer.push(arguments);}
+
+  // gtag('js', new Date());
+
+  // gtag('config', 'UA-43223650-1');
+
 } else {
   console.log('not hotdog');
 }
